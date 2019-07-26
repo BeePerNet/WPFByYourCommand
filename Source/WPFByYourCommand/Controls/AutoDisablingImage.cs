a9d3c6e20672e4a0cb75b39301483d4811a74168
BeePerNet <sebasgariepy@gmail.com>
2019-07-26 08:17:05 -0400

    New VirtualizingWrapPanel with SelectedItemChanged Focus

--- Source/WPFByYourCommand/Controls/AutoDisablingImage.cs
+++ Source/WPFByYourCommand/Controls/AutoDisablingImage.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace WPFByYourCommand.Controls
@@ -24,32 +25,18 @@ namespace WPFByYourCommand.Controls
         /// <param name="args">The <see cref="System.Windows.DependencyPropertyChangedEventArgs"/> instance containing the event data.</param>
         private static void OnAutoGreyScaleImageIsEnabledPropertyChanged(DependencyObject source, DependencyPropertyChangedEventArgs args)
         {
-            if (!args.OldValue.Equals(args.NewValue))
+            try
             {
-                AutoDisablingImage autoGreyScaleImg = source as AutoDisablingImage;
-                if (autoGreyScaleImg != null)
-                    autoGreyScaleImg.Opacity = (!(args.NewValue as bool? ?? false)) ? 0.4 : 1;
-                /*if (autoGreyScaleImg != null && autoGreyScaleImg.Source != null)
+                if (!args.OldValue.Equals(args.NewValue))
                 {
-                    if (!(args.NewValue as bool? ?? false))
-                    {
-                        // Get the source bitmap
-                        var bitmapImage = new BitmapImage(new Uri(autoGreyScaleImg.Source.ToString()));
-                        // Convert it to Gray
-                        autoGreyScaleImg.Source = new FormatConvertedBitmap(bitmapImage, PixelFormats.Gray32Float, null, 0);
-                        // Create Opacity Mask for greyscale image as FormatConvertedBitmap does not keep transparency info
-                        autoGreyScaleImg.OpacityMask = new ImageBrush(bitmapImage);
-                        autoGreyScaleImg.Opacity = 0.5;
-                    }
-                    else
-                    {
-                        // Set the Source property to the original value.
-                        autoGreyScaleImg.Source = ((FormatConvertedBitmap)autoGreyScaleImg.Source).Source;
-                        // Reset the Opcity Mask
-                        autoGreyScaleImg.OpacityMask = null;
-                        autoGreyScaleImg.Opacity = 1;
-                    }
-                }*/
+                    AutoDisablingImage autoGreyScaleImg = source as AutoDisablingImage;
+                    if (autoGreyScaleImg != null)
+                        autoGreyScaleImg.Opacity = (!(args.NewValue as bool? ?? false)) ? 0.4 : 1;
+                }
+            }
+            catch (Exception)
+            {
+
             }
         }
 
