607292fb58707e1b4760a9ad766081775d716d80
BeePerNet <sebasgariepy@gmail.com>
2019-08-07 06:51:19 -0400

    Image IsEqual extension

---
+++ Source/WPFByYourCommand/Converters/ProvideDefaultValueConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WPFByYourCommand.Converters
+{
+    public class ProvideDefaultValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value == null)
+                value = Activator.CreateInstance(targetType);
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
---
+++ Source/WPFByYourCommand/Extensions/BitmapSourceExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using System.Linq;
+using System.Windows.Media.Imaging;
+
+namespace WPFByYourCommand.Extensions
+{
+    public static class BitmapSourceExtensions
+    {
+        public static bool IsEqual(this BitmapSource image1, BitmapSource image2)
+        {
+            if (image1 == null || image2 == null)
+            {
+                return false;
+            }
+            return image1.ToBytes().SequenceEqual(image2.ToBytes());
+        }
+
+        public static byte[] ToBytes(this BitmapSource image)
+        {
+            byte[] data = new byte[] { };
+            if (image != null)
+            {
+                    var encoder = new BmpBitmapEncoder();
+                    encoder.Frames.Add(BitmapFrame.Create(image));
+                    using (MemoryStream ms = new MemoryStream())
+                    {
+                        encoder.Save(ms);
+                        data = ms.ToArray();
+                    }
+                    return data;
+            }
+            return data;
+        }
+    }
+}
