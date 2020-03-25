cf6aa095b31974ace9b11e66e28a0e29cce1f806
BeePerNet <sebasgariepy@gmail.com>
2020-03-25 03:28:37 -0400

    Repaired

--- WPFByYourCommand/Themes/Expander.xaml.cs
+++ WPFByYourCommand/Themes/Expander.xaml.cs
@@ -3,6 +3,8 @@ using System.Windows.Controls;
 
 namespace WPFByYourCommand.Themes
 {
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Naming", "CA1710:Identifiers should have correct suffix", Justification = "<En attente>")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1010:Collections should implement generic interface", Justification = "<En attente>")]
     public partial class Expander: ResourceDictionary
     {
         private void Border_Loaded(object sender, RoutedEventArgs e)
